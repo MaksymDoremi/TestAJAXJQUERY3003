747b91ad7af8c5221d1a53bbd07de68c5f95e137
Maksym Kintor <kintormaksim@gmail.com>
2023-03-30 09:06:08 +0200

    methods

--- script.js
+++ script.js
@@ -16,37 +16,89 @@ let Liked = [];
 let Disliked = [];
 
 function GenerateNewDogFact(){
-	$.ajax({
+	var req = new XMLHttpRequest();
+	req.onreadystatechange = function() {
+		if (this.readyState == 4 && this.status == 200) {
+			console.log('success');
 
-		url: URL,
-		type: "GET",
-		crossDomain: true,
-		dataType: "jsonp",
-		headers: {
-			
-			"Access-Control-Allow-Origin": "*",
-			
-		},
-
-		success: function(data) {
+			let result = JSON.parse(this.response);
 
 
-			let result = JSON.parse(data);
-			console.log(result);
-
-		},
-		error: function(data, status) {
-
+		} else {
+			console.log('error');
 		}
+	}
+	req.open("GET", URL, true);
+	req.send();
+}
 
+function Like(){
+	Liked[Liked.length] = new LikeFacts(
+		$('.factText').val()
+		);
+}
+
+function Dislike(){
+	Disliked[Liked.length] = new DislikeFacts(
+		$('.factText').val()
+		);
+}
+/*
+<div class="card">
+					
+					<div class="card-body">
+						
+						<p class="factText">Facts</p>
+						<a href="#" class="btn btn-primary" onclick="Like()">Like</a>
+						<a href="#" class="btn btn-primary" onclick="Dislike()">Dislike</a>
+						
+					</div>
+				</div>
+
+
+
+*/
+function ShowLiked(){
+	$('#placeForCards').html('');
+
+	Liked.forEach(child => {
+		let card = $('<div></div>');
+		card.addClass('card');
+
+		let cardBody = $('<div></div>');
+		cardBody.addClass('card-body');
+
+		let p = $('<p></p>');
+		p.addClass(factText);
+		p.html(child.fact);
+
+		cardBody.append(p);
+		card.append(cardBody);
+
+		$('#placeForCards').append(card);
 
 	});
+
 }
 
-function Like(fact){
-	Liked[Liked.length] = new LikeFacts(fact);
-}
+function ShowDisliked(){
+	$('#placeForCards').html('');
 
-function Dislike(fact){
-	Disliked[Disliked.length] = new DislikeFacts(fact);
+	Disliked.forEach(child => {
+		let card = $('<div></div>');
+		card.addClass('card');
+
+		let cardBody = $('<div></div>');
+		cardBody.addClass('card-body');
+
+		let p = $('<p></p>');
+		p.addClass(factText);
+		p.html(child.fact);
+
+		cardBody.append(p);
+		card.append(cardBody);
+
+		$('#placeForCards').append(card);
+
+	});
 }
