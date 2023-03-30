45f91530cc9c4f3345f84e78cfe776f7a8407b74
Maksym Kintor <kintormaksim@gmail.com>
2023-03-30 08:42:03 +0200

    help methods

--- script.js
+++ script.js
@@ -2,31 +2,51 @@ const URL = 'http://dog-api.kinduff.com/api/facts';
 
 
 class LikeFacts{
-	constructor(id, img){
-
+	constructor(fact){
+		this.fact = fact;
 	}
 }
 
 class DislikedFacts{
-	constructor(id, img){
-
+	constructor(fact){
+		this.fact = fact;
 	}
 }
+let Liked = [];
+let Disliked = [];
 
 function GenerateNewDogFact(){
 	$.ajax({
-        url: URL,
-        type: "GET", //or POST
-        crossDomain: true,
-        success: function(data) {
+
+		url: URL,
+		type: "GET",
+		crossDomain: true,
+		dataType: "jsonp",
+		headers: {
+			
+			"Access-Control-Allow-Origin": "*",
+			
+		},
+
+		success: function(data) {
 
 
-            let result = JSON.parse(data);
-            console.log(result);
+			let result = JSON.parse(data);
+			console.log(result);
 
-        },
-        error: function(data, status) {
+		},
+		error: function(data, status) {
 
-        }
-    });
+		}
+
+
+	});
+}
+
+function Like(fact){
+	Liked[Liked.length] = new LikeFacts(fact);
+}
+
+function Dislike(fact){
+	Disliked[Disliked.length] = new DislikeFacts(fact);
 }
